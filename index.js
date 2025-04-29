646bc7f29710cbbd95ecce50bf335036fddb63b7
ITrue-dev <den.glushkov.kv@gmail.com>
2025-04-29 12:02:01 +0300

    Test bot commands

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ const token = '8100310935:AAEW1lrN8JYe95zIqG-Su7HWvxel60fSZ4Q';
 
 const bot = new telegramApi(token, {polling: true});
 
+const data = {};
 
-bot.on('message', msg => {
-    console.log(msg)
-})
+const start = () => {
+    bot.setMyCommands([
+        {command: '/start', description: 'Початок спілкування'},
+        {command: '/info', description: 'Інформація'},
+        {command: '/game', description: 'Вгадай число'},
+    ]);
+    
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+        
+        if (text === '/start'){
+            await bot.sendSticker(chatId, 'https://cdn2.combot.org/krutoposmesh1she/webp/1xf09f96bc.webp');   
+            return bot.sendMessage(chatId, `Вітаю в боті FootBot`);
+        }
+        if (text === '/info'){
+            return bot.sendMessage(chatId, `Тебе звати ${msg.from.first_name} ${msg.from.last_name}`);
+        }
+        if (text === '/game'){
+            await bot.sendMessage(chatId, `Я загадав число від 0 до 9`);
+            const randNum = Math.floor(Math.random() * 10);
+            data[chatId] = randNum;
+            return bot.sendMessage(chatId, `Вгадай`);
+        }
+        return bot.sendMessage(chatId, `Напиши щось інше`);
+    });
+};
+
+start();
